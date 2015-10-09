499973d8c5fcafc9cd15689ffe4c94e6c883017a
Willians Nieves <williamsnieves@gmail.com>
2015-10-09 15:11:12 -0430

    deleted version 5.1

--- resources/views/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<section class="container-fluid Footer">
-	<section class="row">
-		<div class="col-xs-12 lesspadding">
-			<p class="Footer-text">CopyRight 2015 - 3WV IT & Digital Media Solutions</p>
-		</div>		
-	</section>
-</section>
-
-
